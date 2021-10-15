49691e4f0a52208f52e0aa09a7556f18e5099033
Nasir Khimani <nasirkhimani@gmail.com>
2021-10-14 23:52:26 -0400

    added mac support started dev

--- final.py
+++ final.py
@@ -1,62 +1,51 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 from json import *
+import os, platform
+with open('password.txt', r) as f:
+    EMAIL = f.read()
+    PW = f.read()
+
+
+# support for macos TODO: add linux when i get raspie
+if platform.system() == 'Darwin':
+    # we need to promote chromedriver
+    os.chmod('defen/chromedriver.exe', 0o755)
+    drivger = r'defen/chromedriver'
+else:
+    drivger = r'defen/chromedriver.exe'
+
 a = []
 options = webdriver.ChromeOptions()
-options.add_argument('headless')
-
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-
-# enable browser logging
-d = DesiredCapabilities.CHROME.copy()
-d['goog:loggingPrefs'] = {'performance': 'ALL'}
+#options.add_argument('headless')
 
 
 
 
-global e
-global driver
-while not a:
-    driver = webdriver.Chrome(executable_path=r"C:\Users\nasir\PycharmProjects\bettermoviebox\chromedriver.exe",
-                              service_args=[r"--verbose",
-                                            r"--log-path=C:\Users\nasir\PycharmProjects\bettermoviebox\chromdriver.log"],
-                              desired_capabilities=d,
-                              options=options)
-    driver.get('https://s2dfree.cc/search/keyword/iron man')
-    btn = driver.find_element_by_id('btn')
+
+driver = webdriver.Chrome(executable_path=drivger,
+                            options=options)
+driver.get('https://mcteam.co/screening-hs')
+try:
+    elem = WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.NAME, "loginfmt"))
+    )
+finally:
+    emil = driver.find_element_by_name('loginfmt')
+    
+    btn = driver.find_element_by_id('idSIButton9')
+    emil.send_keys(EMAIL)
+    sleep(0.5)
+    pw = driver.find_element_by_name('passwd')
+    pw.send_keys = PW
     btn.click()
 
-    number = driver.find_elements_by_xpath('/html/body/div/div[2]/div/div[2]//*[@class = "col-lg-2 col-md-3 col-sm-4 col-xs-6 no-padding"]/div/div[2]/h5/a')
-    print('Loading...' % number)
 
-    for i in number:
-        a.append(i.get_property('innerHTML'))
-    e = len(number)
 
-choices = dict(zip(range(e),a))
-for i in choices.values():
-    print(i)
-choice = input('What movie do you want to watch? ')
 
-mouse = driver.find_element_by_link_text(choices[int(choice)])
-mouse.click()
-
-sleep(2.4)
-
-performance_log = driver.get_log('performance')
-#print(str(performance_log).strip('[]'))
-
-for i in performance_log:
-    p = loads(i['message'])
-    try:
-        path = p['message']['params']['headers'][':path']
-        auth = p['message']['params']['headers'][':authority']
-        if 'wewon.to' in auth:
-            link = auth + path
-            print(link)
-        print(auth)
-    except:
-        pass
 
 
 
