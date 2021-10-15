e768afe90839c97577b00fa5b7ac91c4917143cb
Nasir Khimani <nasirkhimani@gmail.com>
2021-10-15 00:39:01 -0400

    added login.

--- final.py
+++ final.py
@@ -5,11 +5,12 @@ from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 from json import *
 import os, platform
-with open('password.txt', r) as f:
-    EMAIL = f.read()
-    PW = f.read()
+with open('password.pw', 'r') as f:
+    stuff = f.read().split('\n')
+    EMAIL = stuff[0]
+    PW = stuff[1]
 
-
+print(EMAIL,'d', PW)
 # support for macos TODO: add linux when i get raspie
 if platform.system() == 'Darwin':
     # we need to promote chromedriver
@@ -29,19 +30,29 @@ options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(executable_path=drivger,
                             options=options)
 driver.get('https://mcteam.co/screening-hs')
+#step 1: login! 
 try:
     elem = WebDriverWait(driver, 5).until(
     EC.presence_of_element_located((By.NAME, "loginfmt"))
     )
 finally:
     emil = driver.find_element_by_name('loginfmt')
-    
-    btn = driver.find_element_by_id('idSIButton9')
     emil.send_keys(EMAIL)
-    sleep(0.5)
-    pw = driver.find_element_by_name('passwd')
-    pw.send_keys = PW
+    btn = driver.find_element('id','idSIButton9')
     btn.click()
+try:
+    elem = WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.NAME, "passwd"))
+    )
+finally:
+    pw = driver.find_element('name', 'passwd')
+    pw.send_keys(PW)
+    sleep(5)
+    btn2 = driver.find_element('id','idSIButton9')
+    btn2.click()
+
+
+
 
 
 
