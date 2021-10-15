548f128ea1ee1be1f09fa22c778b10cde3fbbc00
Nasir Khimani <nasirkhimani@gmail.com>
2021-10-15 02:01:13 -0400

    added selection. finished login. should be done by tmr

--- final.py
+++ final.py
@@ -24,19 +24,19 @@ options = webdriver.ChromeOptions()
 #options.add_argument('headless')
 
 
-
-
-
 driver = webdriver.Chrome(executable_path=drivger,
-                            options=options)
+                            options=options) # i like this. idc about depreciation
 driver.get('https://mcteam.co/screening-hs')
+
+
+
 #step 1: login! 
 try:
     elem = WebDriverWait(driver, 5).until(
     EC.presence_of_element_located((By.NAME, "loginfmt"))
     )
 finally:
-    emil = driver.find_element_by_name('loginfmt')
+    emil = driver.find_element('name','loginfmt')
     emil.send_keys(EMAIL)
     btn = driver.find_element('id','idSIButton9')
     btn.click()
@@ -47,9 +47,38 @@ try:
 finally:
     pw = driver.find_element('name', 'passwd')
     pw.send_keys(PW)
-    sleep(5)
+    sleep(1)
     btn2 = driver.find_element('id','idSIButton9')
     btn2.click()
+driver.back()
+
+#selection
+try:
+    elem = WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "text-format-content"))
+    )
+finally:
+    pass
+
+qts = 'something'
+d = 1
+while qts:
+    d += 1
+    if d == 7:
+        #answer yes for shorter screening + it applies to me
+        qts = driver.find_element(By.XPATH,'/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[2]/div[7]/div/div[2]/div/div[2]')
+        qts.click()
+    try:
+        #rest answer no
+        qts = driver.find_element(By.XPATH,f'/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[2]/div[{d}]/div/div[2]/div/div[1]')
+        qts.click()
+    except:
+        qts = ''
+    
+try:
+    driver.find_element(By.CLASS_NAME,'button-content')
+except:
+    raise Exception("Something went wrong. Ensure your password is in password.pw")
 
 
 
